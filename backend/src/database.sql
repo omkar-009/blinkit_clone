8b502723c1dd28949516643e57894e66ef9341e9
Omkar-009 <omkarshinde9518@gmail.com>
2025-11-01 00:11:15 +0530

    ok

--- backend/src/database.sql
+++ backend/src/database.sql
@@ -10,12 +10,12 @@ CREATE TABLE IF NOT EXISTS users (
 );
 
 -- home page product table
-CREATE TABLE IF NOT EXISTS products (
+CREATE TABLE IF NOT EXISTS home_page_products (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
     category VARCHAR(50) NOT NULL,
     quantity VARCHAR(50),
     price DECIMAL(10, 2) NOT NULL,
-    image_url VARCHAR(255),
+    images VARCHAR(255),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
